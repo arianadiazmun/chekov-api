293dc986b9b2599966356dc8a636c305e881d6d8
Ariana Diaz <arianadiaz@Arianas-MacBook-Air.local>
2023-07-11 13:48:13 -0400

    WPI update function

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 import { onRequest } from "firebase-functions/v1/https";
 import express from "express";
 import cors from "cors";
-import { getTasks } from "./src/tasks.js";
+import { getTasks, addTask } from "./src/tasks.js";
 
 const app = express ();
 app.use(cors());
--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -1,33 +1,49 @@
 import { FieldValue } from "firebase-admin/firestore";
-import db from "./dbConnect.js"
-
+import db from "./dbConnect.js";
 
 const coll = db.collection("tasks");
+//Get all tasks
+export async function getTasks(req, res) {
+  const { uid } = req.params;
+  //get all tasks by user:
+  const tasks = await coll.where("uid", "==", uid).get(); //uid stands for user id
+  //arranges tasks in an array:
 
-export async function getTasks (req, res) {
-    const { uid } =req.params;
-    //get all tasks by user:
-await coll.where('uid',  '==', uid).get(); //uid stands for user id
-//arranges tasks in an array:
-
-const taskArray = tasks.docs.map(doc => ({id: doc.id, ...doc.data()}));
-res.send(taskArray);
-    
+  const taskArray = tasks.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  res.send(taskArray);
 }
 
-export async function addTask (req, res ){
-    const {title, uid} = req.body;
-    if(!title || !uid) {
-        res.status(401).send({succes: false, message: 'Not a valid request'});
-        return;
+export async function addTask(req, res) {
+  const { title, uid } = req.body;
+  if (!title || !uid) {
+    res.status(401).send({ succes: false, message: "Not a valid request" });
+    return;
+  }
+  const newTask = {
+    title,
+    uid,
+    done: false,
+    createdAt: FieldValue.serverTimestamp(),
+  };
+  await coll.add(newTask);
+  getTasks(req, res);
+}
 
-    }
-    const newTask = {
-        title,
-        uid, done: false,
-        createdAt: FieldValue.serverTimestamp(),
+// update tasks
+export async function updateTask (req,res) {
+  const {done, uid}= req.body;
+  
+  if(!uid) {
+    res.status(401).send({success: false, message: "Not a valid request"});
+    return;
+  }
 
-    }
-    await coll.add(newTask);
-    getTasks(req, res);
+  const updates= {
+    done,
+    updatedAt: FieldValue.serverTimestamp()
+  }
+
+  await coll.doc(uid).update(updates);
+
+  getTasks(req, res);
 }
